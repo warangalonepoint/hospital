11b8195ad1f381df97c74906b241e6ed15b060af
warangalonepoint <gullapallychaitanya@gmail.com>
2025-09-10 07:11:29 +0530

    Update config.js

--- dr-charan-hospital-pwa/js/config.js
+++ dr-charan-hospital-pwa/js/config.js
@@ -1,3 +1,5 @@
 // FILL THESE with your project (Project Settings → API)
-export const SUPABASE_URL = "https://YOUR-PROJECT.ref.supabase.co";
-export const SUPABASE_ANON_KEY = "YOUR-ANON-KEY";
+export const SUPABASE_URL = "https://vhlftajwsmfwvkuvznpz.supabase.co
+";
+export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZobGZ0YWp3c21md3ZrdXZ6bnB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0NjgxNzgsImV4cCI6MjA3MzA0NDE3OH0.CFwMbpjgJyLUY_l_QuQd0SU9Qp0TOfHiUOCostcRUqA
+";
