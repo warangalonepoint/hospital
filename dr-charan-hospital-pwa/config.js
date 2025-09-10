a7ecf6e86cc4802e07c35bd47a03979256b46b96
warangalonepoint <gullapallychaitanya@gmail.com>
2025-09-10 14:40:41 +0530

    Update config.js

--- dr-charan-hospital-pwa/config.js
+++ dr-charan-hospital-pwa/config.js
@@ -3,5 +3,5 @@
 
 export const SUPABASE_URL = "https://vhllftajwsmfwvkuvznpz.supabase.co";
 export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZobGZ0YWp3c21md3ZrdXZ6bnB6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc0NjgxNzgsImV4cCI6MjA3MzA0NDE3OH0.CFwMbpjgJyLUY_l_QuQd0SU9Qp0TOfHiUOCostcRUqA
-"; 
+"
 
